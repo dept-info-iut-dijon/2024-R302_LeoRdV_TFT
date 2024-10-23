43d21d4005c32a2b5fcf40c6067282921d3db53a
LeoRdV <leo.richard-de-vesvrotte@iut-dijon.u-bourgogne.fr>
2024-10-23 14:44:38 +0200

    Now everything actually works for the first view of the template

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ require_once "Helpers\Psr4AutoLoaderClass.php";
 $loader = new Helpers\Psr4AutoloaderClass();
 $loader->register();
 $loader->addNamespace('\Helpers', '/Helpers');
-$loader->addNamespace('\League\Plates', '/Vendor/Plates/src');
+$loader->addNamespace('\League\Plates', '/Vendor/plates/src');
 
-//$engine = new Engine();
-//echo $engine->render("template.php", array());
+$engine = new League\Plates\Engine(__DIR__."/Views");
+echo $engine->render("home", ['tftSetName' => "truc"]);
 
 
