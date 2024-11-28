a31575d4d98206d912ef0e04b8ff32c5dfe5a1a1
LeoRdV <leo.richard-de-vesvrotte@iut-dijon.u-bourgogne.fr>
2024-11-28 15:35:45 +0100

    BasePDODAO fonctionne ! (maybe)

--- Models/BasePDODAO.php
+++ Models/BasePDODAO.php
@@ -3,10 +3,10 @@ use Config\Config;
 abstract class BasePDODAO{
     private $db;
 
-    public function __construct(){
-       
-    }
-
+    /**
+     * Creates DB connection 
+     * @return PDO
+     */
     public function getDB():PDO{
         if (is_null($this->db)){
             
@@ -16,6 +16,20 @@ abstract class BasePDODAO{
             Config::get('pass'));
         }
         return $this->db;
+    }
+/**
+ * Executes SQL requests 
+ * @param string $sql the SQL request with "?" where you need params
+ * @param array $params Array of parameters, must be in the same order as the "?", null by default
+ * @return PDOStatement what the request is showing
+ * @return false if no PDOStatement
+ */
+    protected function execRequest(string $sql, array $params = null):PDOStatement|false{
+        $stmt = htmlspecialchars($sql);
+        $req = $this->db->prepare($sql);
+        $bool = $req->execute($params);
+        if ($bool) return $req;
+        else return false;
     }
 
 }
