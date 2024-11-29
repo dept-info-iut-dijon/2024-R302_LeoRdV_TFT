1971525a424a4bfd1f2a03356658c01c55772a41
LeoRdV <leo.richard-de-vesvrotte@iut-dijon.u-bourgogne.fr>
2024-11-29 16:37:44 +0100

    created class route

--- Controllers/Router/route.php
+++ Controllers/Router/route.php
@@ -1,4 +1,19 @@
 <?php
 abstract class Route{
-    
+    public function action(array $param = [], string $method = 'GET'): void{
+        if ($method == 'POST') return $this->post($param);
+        else return $this->get($param);
+    }
+
+    protected function getParam(array $array, string $paramName, bool $canBeEmpty=true)
+    {
+        if (isset($array[$paramName])) {
+            if(!$canBeEmpty && empty($array[$paramName]))
+                throw new Exception("Paramètre '$paramName' vide");
+            return $array[$paramName];
+        } else
+            throw new Exception("Paramètre '$paramName' absent");
+    }
+    public abstract function get(array $param =[]):void;
+    public abstract function post(array $param =[]):void;
 }
